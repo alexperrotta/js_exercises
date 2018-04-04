25a49c6697d86b4036019df6426c450cc2e232cb
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-04 19:31:14 -0400

    changes for NIM

---
+++ nim/nim.js
@@ -0,0 +1,21 @@
+console.log("Hello. Let's play NIM.");
+
+/* Instructions:
+Here are the rules for NIM:
+
+There are two players that take turns
+There are 16 rocks in a pile between the two players
+On your turn, you can choose to remove 1, 2, or 3 rocks from the pile. After you make your choice, 
+your turn is over
+The winner is the player that takes the last rock from the pile  */
+
+var board = ****************;
+
+var turn = "player 1";
+
+function printBoard() {
+	console.log(board);
+}
+
+
+
