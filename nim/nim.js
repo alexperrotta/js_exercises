6cfb3a7aa4cbb4ee8edbb0ea391d98d39a1e40c8
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-04 20:56:36 -0400

    more changes

--- nim/nim.js
+++ nim/nim.js
@@ -9,13 +9,37 @@ On your turn, you can choose to remove 1, 2, or 3 rocks from the pile. After you
 your turn is over
 The winner is the player that takes the last rock from the pile  */
 
-var board = ****************;
+var rocks = '* * * * * * * * * * * * * * * *';
 
-var turn = "player 1";
+var turn = 'player 1';
 
-function printBoard() {
-	console.log(board);
+function printRocks() {
+	console.log(rocks);
 }
+
+
+function takeRocks() {
+	var playerPrompt = parseInt(prompt('How many rocks do you want to take? You can only take 1, 2, or 3 rocks from the pile.'));
+
+	if (playerPrompt === '1') {
+		;
+	} else if (playerPrompt === '2') {
+		;
+	} else if (playerPrompt === '3') {
+		;
+	} 
+	
+	console.log(printRocks());
+}
+
+takeRocks();
+
+
+
+function checkVictory() {
+
+}
+
 
 
 
