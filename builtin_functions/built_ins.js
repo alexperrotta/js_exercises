f8b78d7a8a3ebeaecda90c9ac917e174032c2344
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-07 10:41:30 -0400

    bonus exercises

--- builtin_functions/built_ins.js
+++ builtin_functions/built_ins.js
@@ -192,7 +192,17 @@ the respective sums for each inner array is 8, 20, and 9. Therefore, the solutio
   [2, 4, 6, 8]
 ]  */
 
+var arr = [
+  [1, 3, 4],
+  [2, 4, 6, 8],
+  [3, 6]
+];
 
+var sortedNames = people.sort(function(a, b){
+	return sum(a.length - b.length);
+});
+
+console.log(sortedNames);
 
 
 
--- js_exercises.js
+++ js_exercises.js
@@ -299,3 +299,71 @@ console.log(newArray);
 
 
 
+/* Bonus: forEach
+Implement your own custom forEach function which takes two arguments: an array arr and a function fun. 
+It will call fun passing each item in arr to fun as the first argument. Example:
+
+var arr = [
+  { name: 'Bob' },
+  { name:'Alice' },
+  { name:'Joe' }
+];
+
+forEach(arr, function(person) {
+  console.log('Hello, ' + person.name + '!');
+});
+The above program will print:
+
+Hello, Bob!
+Hello, Alice!
+Hello, Joe!
+You can use a loop in the implementation of this function.  */
+
+var forEach = function(arr, callback) {
+// loop over the input array, call the callback on each item of the array
+  for (var i = 0; i < array.length; i++) {
+    callback(arr[i]);
+  }
+}
+
+
+
+forEach([1, 2, 3], function(item){  // the array is the 'arr' parameter and the function(item) is the callback
+  console.log(item);
+});
+
+
+
+
+/* Bonus: map
+Implement your own custom map function which takes two arguments: an array arr and a function fun. 
+It will return a new array, with each of its results being the result of calling fun with each array element. */
+
+var map = function(arr, callback) {
+  // return a new array
+  // each new item should be the result of the callback, passing in an individual item
+  var array = [];
+
+   for (var i = 0; i < array.length; i++) {
+    var item = arr[i];
+    var newItem = callback(item);
+    newArray.push(newItem);
+  }
+
+  return newArray;
+}
+
+var result = map([1, 2, 3], function(item){
+  return item * item;
+})
+
+console.log(result);
+
+
+
+
+
+
+
+
+
